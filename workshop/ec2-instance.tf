d5f968837533f58baef7f35d17742e394dcc8bb9
sagarmate <smate@equalexperts.com>
2019-12-07 15:31:08 +0530

    added instance details

--- workshop/ec2-instance.tf
+++ workshop/ec2-instance.tf
@@ -3,10 +3,11 @@ resource "aws_key_pair" "ssh_key" {
   public_key = ""
 }
 
-resource "aws_instance" "first_instance" {
-  ami = ""
 
-  subnet_id = ""
+resource "aws_instance" "first_instance" {
+  ami = "ami-04b9e92b5572fa0d1"
+
+  subnet_id = "subnet-0d26fa40"
 
   key_name = aws_key_pair.ssh_key.key_name
 
